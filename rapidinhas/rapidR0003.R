5dd89c14580a8ccbe2f2536fd2f5530b72adf60e
bendeivide <ben.deivide@gmail.com>
2021-03-30 14:18:03 -0300

    rapidR003.R e rapidR0004.R

---
+++ rapidinhas/rapidR0003.R
@@ -0,0 +1,20 @@
+###########################################################
+# Rapidinhas do R
+# Canal: http://youtube.com/bendeivide
+# Repositorio: https://github.com/bendeivide/rapidinhasdor
+#----------------------------------------------------------
+# RapidR 0003 - Atribuicao ou superatribuicao
+#----------------------------------------------------------
+# Prof. Ben Dêivide (http://bendeivide.github.io/)
+########################################################### 
+
+# Atribuicao
+x <- 10
+
+# Superatribuicao 
+
+fx <- function() {
+   x <<- 20
+}
+
+fx()
---
+++ rapidinhas/rapidR0004.R
@@ -0,0 +1,18 @@
+###########################################################
+# Rapidinhas do R
+# Canal: http://youtube.com/bendeivide
+# Repositorio: https://github.com/bendeivide/rapidinhasdor
+#----------------------------------------------------------
+# RapidR 0004 - Acessando funcoes internas de um pacote
+#----------------------------------------------------------
+# Prof. Ben Dêivide (http://bendeivide.github.io/)
+########################################################### 
+
+# Instalar
+install.packages("SMR")
+
+# Anexando
+Library(SMR)
+
+# Carregamento
+SMR:::GaussLegendre(4)
